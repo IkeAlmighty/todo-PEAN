9d2b21baf55682aaad67b0d3dda151ae3465c941
ikealmighty <isaacyates7@gmail.com>
2025-02-10 15:05:27 -0600

    refractored to recommended setup because of async await issues

--- server/src/db/connect.js
+++ server/src/db/connect.js
@@ -1,35 +1,21 @@
 import { Sequelize } from 'sequelize';
 
-const sequelizeInstance = undefined;
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 
-async function getSequelize() {
-    // lazy init sequelizeInstance
-    if (!sequelizeInstance) {
-        sequelizeInstance = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-            dialect: 'postgres',
-            host: 'localhost'
-        });
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    dialect: 'postgres',
+    host: 'localhost'
+});
 
-        try {
-            await sequelizeInstance.authenticate();
-            console.log('Database authenticated successfully')
-        } catch (err) {
-            console.log("Unable to connect to database: ", err);
-        }
-    }
-
+async function syncronize() {
     // sync
-
     try {
-        await sequelizeInstance.sync({ force: true });
+        await sequelize.sync({ force: true });
         console.log('Database synchonized successfully')
-
-        return sequelizeInstance;
     } catch (err) {
         console.log("Unable to synchronize database: ", err);
     }
 }
 
-export { getSequelize }
+export { syncronize, sequelize }
