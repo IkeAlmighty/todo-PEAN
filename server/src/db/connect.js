be9f6172ffb531f56f5cd8399c39c8e394c73a8d
ikealmighty <isaacyates7@gmail.com>
2025-02-10 16:10:27 -0600

    added dotenv call so that sequelize as access to env variables

--- server/src/db/connect.js
+++ server/src/db/connect.js
@@ -1,4 +1,6 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
