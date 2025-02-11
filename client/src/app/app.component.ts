ec242e9567cca41c8ccc4ac0670673176e783032
ikealmighty <isaacyates7@gmail.com>
2025-02-11 12:44:35 -0600

    rewrote app component for todo list

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoContainerComponent } from './components/todo-container/todo-container.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TodoContainerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppComponent {
   title = 'client';
