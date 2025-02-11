0dbcdc7fc5f5a176ce4cb676a15a1e518e6f1cbe
ikealmighty <isaacyates7@gmail.com>
2025-02-11 12:45:20 -0600

    removed ts in favor of postgres created ts

--- server/src/models/todo.js
+++ server/src/models/todo.js
@@ -1,18 +1,11 @@
-import { sequelize } from '../db/connect.js';
-import { DataTypes } from 'sequelize';
+import { sequelize } from "../db/connect.js";
+import { DataTypes } from "sequelize";
 
-const Todo = sequelize.define(
-    'Todo',
-    {
-        text: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        ts: {
-            type: DataTypes.TIME,
-            allowNull: false
-        }
-    }
-);
+const Todo = sequelize.define("Todo", {
+  text: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
 export default Todo;
